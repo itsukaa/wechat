a333cd63b911089c83796bfca4a032a9dee55799
Itsukaa <82749852+itsukaa@users.noreply.github.com>
2022-08-28 18:19:10 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wxb10f05b413084440",
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "b253d2bbb6de651e461b2228bfd08e2c",
     // 模板消息id
-    TEMPLATE_ID: "",
+    TEMPLATE_ID: "VaXQj4IPrE8LHlGh5EEGvcHOSfHKem6eZ2TzOC6iwAI",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    USERS: [""],
+    USERS: ["ogPI16pyZkEGWB3-0OP1UAZxt-1E"],
      
     /**
      * 信息配置
@@ -19,31 +19,30 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "湖北",
     // 所在城市
-    CITY: "惠州",
+    CITY: "武汉",
 
     /** 生日相关 */
 
     // 生日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
     BIRTHDAYS: [
-      {"name": "老婆", "year": "1993", "date": "12-27"},
-      {"name": "家公", "year": "1993", "date": "08-09"},
+      {"name": "花花", "year": "1993", "date": "12-27"},
     ],
 
     /** 日期相关 */
 
     // 在一起的日子，格式同上
-    LOVE_DATE: "2015-05-01",
+    LOVE_DATE: "2016-08-20",
     // 结婚纪念日
-    MARRY_DATE: "2020-01-04",
+    MARRY_DATE: "2025-01-04",
 
 
     /** 好文节选 */
 
     // 好文节选的内容类型
     // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-    LITERARY_PREFERENCE: ""
+    LITERARY_PREFERENCE: "第四步：启用workflow自动任务,定时执行"
 
 
     }
